463681727dc9fe7ce7245589453a89458396beb8
SivaTeja-20 <99390827+SivaTeja-20@users.noreply.github.com>
2022-03-15 17:52:51 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var str = "Skill Safari"
+console.log(str)
+console.log("length of str is : "+str.length)
+console.log(str.toUpperCase("str"))
+console.log("Index of \"a\" in str : " +str.indexOf("a"))
+
+console.log("Steve Rogers : Big man in a suit of armour. Take that off, what are you?\nTony Stark : Genius, billionaire, playboy, philanthropist.\nSteve Rogers : I know guys with none of that worth ten of you. I've seen the footage. The only thing you really fight for is yourself. You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you.\nTony Stark : I think I would just cut the wire.\nSteve Rogers : Always a way out... You know, you may not be a threat, but you better stop pretending to be a hero.\nTony Stark : A hero? Like you? You're a lab rat, Rogers. Everything special about you came out of a bottle!\nSteve Rogers : Put on the suit. Let's go a few rounds.")
+
+// BMI
+//height in meter and weight in kg
+var height = 1.82
+var weight = 65
+var BMI = weight/(height*height)
+console.log("BMI : "+BMI)
